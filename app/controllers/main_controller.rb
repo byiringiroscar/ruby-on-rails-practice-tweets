c1eb9e1bb9587aa291d2274b0ad8267deb84cac3
byiringiroscar <byiringoroscar@gmail.com>
2023-06-19 12:53:29 +0200

    Setup session in blowser after signup

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,8 @@ class MainController < ApplicationController
     def index
         # flash[:notice] = "Logged in successfully"
         # flash[:alert] = "Invalid email or password"
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        end
     end
 end
