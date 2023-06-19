873a9465d564a29edf811395d82729def57176ef
byiringiroscar <byiringoroscar@gmail.com>
2023-06-19 12:41:47 +0200

    Add setup and user registration

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "main#index"
   get "about-us", to: "about#index", as: :about
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end
