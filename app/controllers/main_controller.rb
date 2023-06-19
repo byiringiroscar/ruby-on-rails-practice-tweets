aad962816dbbb67304914ad70953f701d9ab1f4c
byiringiroscar <byiringoroscar@gmail.com>
2023-06-19 13:07:47 +0200

    Log out session

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
         # flash[:notice] = "Logged in successfully"
         # flash[:alert] = "Invalid email or password"
         if session[:user_id]
-            @user = User.find(session[:user_id])
+            @user = User.find_by(id: session[:user_id])
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
 end
